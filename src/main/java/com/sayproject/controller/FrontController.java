53307b56932a4003471ba1ddf975990fdeb5606c
ByeongHoonChoi <sentimentalhoon@gmail.com>
2023-09-05 12:07:12 +0900

    say basic complete

---
+++ src/main/java/com/sayproject/controller/FrontController.java
@@ -0,0 +1,30 @@
+package com.sayproject.controller;
+
+import java.io.IOException;
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class FrontController
+ */
+@WebServlet(urlPatterns = {"*.say"})
+public class FrontController extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		String requestURI = request.getRequestURI();
+		String contextPath = request.getContextPath();
+		String subStringRequestURI = requestURI.substring(contextPath.length());
+		
+		System.out.println(subStringRequestURI);
+		
+		if (subStringRequestURI.equals("/Main.say")){
+		  RequestDispatcher rd = request.getRequestDispatcher("/WEB-INF/main.jsp");
+		  rd.forward(request, response);
+		}
+	}
+}
