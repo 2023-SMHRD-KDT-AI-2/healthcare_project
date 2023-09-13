d24790b8ec98f3fecf5ae419352b4f474a120ef2
ByeongHoonChoi <sentimentalhoon@gmail.com>
2023-09-13 13:55:56 +0900

    프로젝트 모든 파일 삭제

--- src/main/java/com/sayproject/model/Main/MapperXML/package-info.java
+++
@@ -1,1 +0,0 @@
-package com.sayproject.model.Main.MapperXML;
--- src/main/java/com/sayproject/model/Main/mongoQuickStart.java
+++
@@ -1,7 +0,0 @@
-package com.sayproject.model.Main;
-
-public class mongoQuickStart {
-  public static void main(String[] args) {
-
-  }
-}
