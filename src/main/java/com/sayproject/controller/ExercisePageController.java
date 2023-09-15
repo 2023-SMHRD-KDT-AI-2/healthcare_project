61faf7189e1a5c4bc235013c3ea938e7e4037727
ByeongHoonChoi <sentimentalhoon@gmail.com>
2023-09-15 09:58:01 +0900

    error complete

--- src/main/java/com/sayproject/controller/ExercisePageController.java
+++ src/main/java/com/sayproject/controller/ExercisePageController.java
@@ -14,36 +14,30 @@ import com.sayproject.controller.Main.LoginAction;
 import com.sayproject.controller.Main.MainAction;
 
 public class ExercisePageController {
-	protected void doProcess(HttpServletRequest request, HttpServletResponse response)
-			throws ServletException, IOException {
-		String cmd = "default";
-		if (request.getParameter("c") != null) {
-			cmd = request.getParameter("c");
-			Action action = router(cmd, response);
-			action.execute(request, response);
-		} else {
-			Action action = router(cmd, response);
-			action.execute(request, response);
-		}
-	}
+  protected void doProcess(HttpServletRequest request, HttpServletResponse response)
+      throws ServletException, IOException {
+    String cmd = "default";
+    if (request.getParameter("c") != null) {
+      cmd = request.getParameter("c");
+      Action action = router(cmd, response);
+      action.execute(request, response);
+    } else {
+      Action action = router(cmd, response);
+      action.execute(request, response);
+    }
+  }
 
-	public Action router(String cmd, HttpServletResponse response) {
-		if (cmd.equals("main")) {
-			return new ExerciseMainAction();
+  public Action router(String cmd, HttpServletResponse response) {
+    if (cmd.equals("main")) {
+      return new ExerciseMainAction();
 
-		} else if (cmd.equals("sub1")) {
-			return new ExerciseSub1Action();
-		} else if (cmd.equals("sub2")) {
-			return new ExerciseSub2Action();
-		} else if (cmd.equals("inputInfo")) {
+    } else if (cmd.equals("sub1")) {
+      return new ExerciseSub1Action();
+    } else if (cmd.equals("sub2")) {
+      return new ExerciseSub2Action();
+    } else if (cmd.equals("inputInfo")) {
       return new MemberExerciseInformationInput();
-    } 
-		// MariaDB와 MonggoDB에서 데이터 주기 위한 Action
-		// 해당 로직이 실행되도 GetExerciseMETAction()을 실행하면
-		// ajax를 통해 Diet.say를 요청하기 때문에 Diet.say로 페이지가 넘어감
-		else if (cmd.equals("exerciseCode")) {
-		      return new GetExerciseMETAction();
-		    } 
-		return new ExerciseMainAction();
-	}
+    }
+    return new ExerciseMainAction();
+  }
 }
