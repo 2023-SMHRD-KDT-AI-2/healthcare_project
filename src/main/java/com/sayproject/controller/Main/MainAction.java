43bb9ffb7d015152de6ddce180bc3ce6e4ba791a
ByeongHoonChoi <sentimentalhoon@gmail.com>
2023-09-19 14:21:45 +0900

    error 수정

--- src/main/java/com/sayproject/controller/Main/MainAction.java
+++ src/main/java/com/sayproject/controller/Main/MainAction.java
@@ -32,59 +32,60 @@ public class MainAction implements Action {
         int trainerMemberCount = dao.getTrainerMemberCount(trainerObjectId);
         httpSession.setAttribute("trainerMemberCount", trainerMemberCount);
 
-    //남녀 비율
-    MembersDAO allDao = new MembersDAO();
-    
-    int mCnt = allDao.male("m");
-    int fCnt = allDao.female("f");
-    
-    request.setAttribute("male", mCnt);
-    request.setAttribute("female", fCnt);
-    
-    System.out.println("남자확인:"+mCnt);
-    System.out.println("여자확인:"+fCnt);
-    
-    //나이별비율
-    
-    // 10대
-    int teenCnt = allDao.teenage();
-   
-   request.setAttribute("teenage", teenCnt);
-   
-    // 20대
-    int twoCnt = allDao.twenty();
-   
-   request.setAttribute("twenty", twoCnt);
-    
-    // 30대
-    int thirCnt = allDao.thirty();
-   
-   request.setAttribute("thirty", thirCnt);
-    
-    // 40대
-    int forCnt = allDao.forty();
-   
-   request.setAttribute("forty", forCnt);
-    
-    // 50대
-    int fifCnt = allDao.fifty();
-   
-   request.setAttribute("fifty", fifCnt);
-   
-   // 60대
-   int sixCnt = allDao.sixty();
-   
-   request.setAttribute("sixty", sixCnt);
-   
-   // 70대
-   int sevenCnt = allDao.seventy();
-   
-   request.setAttribute("seventy", sevenCnt);
-    
-   
-    
-    
-    RequestDispatcher dis = request.getRequestDispatcher("WEB-INF/views/Main/main.jsp");
-    dis.forward(request, response);
+        // 남녀 비율
+        MembersDAO allDao = new MembersDAO();
+
+        int mCnt = allDao.male("m");
+        int fCnt = allDao.female("f");
+
+        request.setAttribute("male", mCnt);
+        request.setAttribute("female", fCnt);
+
+        System.out.println("남자확인:" + mCnt);
+        System.out.println("여자확인:" + fCnt);
+
+        // 나이별비율
+
+        // 10대
+        int teenCnt = allDao.teenage();
+
+        request.setAttribute("teenage", teenCnt);
+
+        // 20대
+        int twoCnt = allDao.twenty();
+
+        request.setAttribute("twenty", twoCnt);
+
+        // 30대
+        int thirCnt = allDao.thirty();
+
+        request.setAttribute("thirty", thirCnt);
+
+        // 40대
+        int forCnt = allDao.forty();
+
+        request.setAttribute("forty", forCnt);
+
+        // 50대
+        int fifCnt = allDao.fifty();
+
+        request.setAttribute("fifty", fifCnt);
+
+        // 60대
+        int sixCnt = allDao.sixty();
+
+        request.setAttribute("sixty", sixCnt);
+
+        // 70대
+        int sevenCnt = allDao.seventy();
+
+        request.setAttribute("seventy", sevenCnt);
+
+
+
+        RequestDispatcher dis = request.getRequestDispatcher("WEB-INF/views/Main/main.jsp");
+        dis.forward(request, response);
+      }
+    }
   }
 }
